a0d308b0514daf9c2c7f1150b71f9a5a00a9d608
JohnQuimson <john.quimson777@gmail.com>
2024-06-13 15:25:04 +0200

    feat: add some icons

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,23 @@
 import './Card.scss';
 // import blogImage from '../../assets/image.png';
-import { FaInfo, FaLongArrowAltRight } from 'react-icons/fa';
+
+import {
+  FaInfo,
+  FaLongArrowAltRight,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaPhp,
+} from 'react-icons/fa';
 
 const Card = ({ title, imageUrl, content, tags, published }) => {
+  const tagIcons = {
+    html: <FaHtml5 />,
+    css: <FaCss3Alt />,
+    js: <FaJs />,
+    php: <FaPhp />,
+  };
+
   return (
     <>
       <div className={`card ${published ? 'published' : ''}`}>
@@ -20,7 +35,8 @@ const Card = ({ title, imageUrl, content, tags, published }) => {
             {/* Set per rimuovere i duplicati */}
             {[...new Set(tags)].map((tag, i) => (
               <li key={`tag${i}`}>
-                <span className={tag}>- {tag}</span>
+                <span className={`tag-icon ${tag}`}>{tagIcons[tag]}</span>
+                <span className={`tag-name ${tag}`}>{tag}</span>
               </li>
             ))}
           </ul>
